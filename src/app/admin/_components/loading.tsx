a546ac51e1de91a98797d52613185bee0bf01aa6
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-10-23 10:50:18 -0300

    style: adjust spinner styles

--- src/app/admin/_components/loading.tsx
+++ src/app/admin/_components/loading.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div className="bg-white flex justify-center items-center h-screen">
+    <div className="absolute top-0 left-0 bg-white/50 backdrop-blur-sm flex justify-center items-center h-screen w-full z-[999]">
       <Image src="/loading-spinner.svg" alt="Loading" width={50} height={50} />
     </div>
   );
