c9f0ac4a3ab55a5033823220f2eb586e6008ceab
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-10-23 15:43:59 -0300

    fix: adjust sections and preview

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -16,13 +16,63 @@ import SideBar from "./_components/sideBar";
 import { useSession } from "next-auth/react";
 import Loading from "./_components/loading";
 import HeaderAdmin from "./_components/header";
+import { useCallback, useEffect, useState } from "react";
+import { getCourses } from "@/http/courses/get-courses";
+import TableWithPagination from "./_components/table-with-pagination";
+import Link from "next/link";
+
+type Course = {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  duration: number;
+  starred: boolean;
+  instructor: string;
+};
 
 export default function AdminDashboard() {
   const { status } = useSession({
     required: true,
   });
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  const fetchCourses = useCallback(async () => {
+    const coursesFn = await getCourses({
+      page: 1,
+      perPage: 5,
+    });
+
+    const data = await coursesFn.json();
+
+    const courses = data.courses;
+
+    setCourses(courses);
+  }, []);
+
+  useEffect(() => {
+    fetchCourses();
+  }, [fetchCourses]);
+
+  const filteredCourses = courses.filter(
+    (course) =>
+      course.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      course.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      course.instructor.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   if (status === "loading") return <Loading />;
+
+  const handleDeleteCourse = async (id: string) => {
+    console.log(id);
+  };
+
+  const handleSelectCourseForEdit = async (id: string) => {
+    console.log(id);
+  };
 
   return (
     <div className="flex h-screen bg-gray-100">
@@ -35,74 +85,53 @@ export default function AdminDashboard() {
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
           <div className="container mx-auto px-6 py-8">
             <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-              Overview
+              Informações Gerais
             </h2>
-            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-3">
               <StatCard
-                title="Total Users"
+                title="Total de Cursos"
                 value="6,389"
-                icon={<Users className="h-8 w-8 text-blue-600" />}
+                icon={<Layers className="h-8 w-8 text-blue-600" />}
               />
               <StatCard
-                title="New Users"
+                title="Novos Cursos"
                 value="120"
-                icon={<Users className="h-8 w-8 text-green-600" />}
+                icon={<Layers className="h-8 w-8 text-green-600" />}
               />
               <StatCard
-                title="Active Sessions"
+                title="Total de usuários"
                 value="1,437"
-                icon={<Layers className="h-8 w-8 text-yellow-600" />}
-              />
-              <StatCard
-                title="Server Load"
-                value="68%"
-                icon={<BarChart className="h-8 w-8 text-red-600" />}
+                icon={<Users className="h-8 w-8 text-yellow-600" />}
               />
             </div>
 
             <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-              Recent Users
+              Cursos Recentes
             </h2>
             <Card>
               <CardHeader>
-                <CardTitle>User Management</CardTitle>
+                <CardTitle>Cursos</CardTitle>
               </CardHeader>
               <CardContent>
                 <div className="flex justify-between items-center mb-4">
-                  <Input className="max-w-sm" placeholder="Search users..." />
-                  <Button>Add User</Button>
+                  <Button>
+                    <Link href="/admin/courses">Ver todos</Link>
+                  </Button>
                 </div>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Name</TableHead>
-                      <TableHead>Email</TableHead>
-                      <TableHead>Role</TableHead>
-                      <TableHead>Actions</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {users.map((user) => (
-                      <TableRow key={user.id}>
-                        <TableCell>{user.name}</TableCell>
-                        <TableCell>{user.email}</TableCell>
-                        <TableCell>{user.role}</TableCell>
-                        <TableCell>
-                          <Button variant="ghost" size="sm">
-                            Edit
-                          </Button>
-                          <Button
-                            variant="ghost"
-                            size="sm"
-                            className="text-red-600"
-                          >
-                            Delete
-                          </Button>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
+                <TableWithPagination
+                  contents={filteredCourses}
+                  headers={[
+                    { key: "name", value: "Nome" },
+                    { key: "description", value: "Descrição" },
+                    { key: "instructor", value: "Instrutor" },
+                  ]}
+                  handleDelete={handleDeleteCourse}
+                  handleEdit={handleSelectCourseForEdit}
+                  isLoading={isLoading}
+                  isActions={false}
+                  starred={false}
+                  isPagination={false}
+                />
               </CardContent>
             </Card>
           </div>
